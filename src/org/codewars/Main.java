81d5db393b72e8aab4ea1ed0c798e78a986dd910
Decagon <decagon@Decagons-MacBook-Air.local>
2022-01-26 14:58:54 +0100

    Reverse word
    Given a sentence reverse each word that has more than four characters

---
+++ src/org/codewars/Main.java
@@ -0,0 +1,28 @@
+package org.codewars;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        spinWords("Hey fellow warriors");
+    }
+    public static String spinWords(String word){
+        StringBuffer stringBuffer;
+        String[] words = word.split(" ");
+        String[] output = new String[words.length];
+        for (int i = 0; i < words.length ; i++) {
+            if(words[i].length() >= 5) {
+                stringBuffer = new StringBuffer(words[i]);
+                stringBuffer.reverse();
+                output[i] = stringBuffer.toString();
+            }
+            else
+                output[i] = words[i];
+        }
+        String.join(" ", output);
+        return String.join(" ", output);
+    }
+}
